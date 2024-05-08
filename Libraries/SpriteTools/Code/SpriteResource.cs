43bf449399d158d12906bd42e82f97fcd0cdc935
Carson Kompon <carsokompo@gmail.com>
2024-05-08 15:45:01 -0400

    Let the console yell at me for now. I'll make an issue for this later

--- Libraries/SpriteTools/Code/SpriteResource.cs
+++ Libraries/SpriteTools/Code/SpriteResource.cs
@@ -21,9 +21,9 @@ public class SpriteAnimation
 {
 	[Property, Title("Animation Name")] private string _nameProp => Name;
 	public string Name { get; set; }
-	[Property, Range(0f, 999f, 0.01f, true, false), DefaultValue(15f)] public float FrameRate { get; set; }// = 15.0f;
-	[Property, Range(0f, 1f, 0.01f, true, false), DefaultValue(0.5f)] public Vector2 Origin { get; set; }// = new Vector2(0.5f, 0.5f);
-	[Property] public bool Looping { get; set; } = true;
+	[Property, Range(0f, 999f, 0.01f, true, false), DefaultValue(15f)] public float FrameRate { get; set; } = 15.0f;
+	[Property, Range(0f, 1f, 0.01f, true, false), DefaultValue(0.5f)] public Vector2 Origin { get; set; } = new Vector2(0.5f, 0.5f);
+	[Property, DefaultValue(true)] public bool Looping { get; set; } = true;
 	public List<string> Frames { get; set; }
 
 	public SpriteAnimation()
