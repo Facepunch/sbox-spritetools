3faf2e2b4a2e83b54ba86b9b3d11a4b18e804388
Carson Kompon <carsokompo@gmail.com>
2024-05-08 16:26:05 -0400

    Added some light documentation to SpriteResouce & SpriteAnimation

--- Libraries/SpriteTools/Code/SpriteResource.cs
+++ Libraries/SpriteTools/Code/SpriteResource.cs
@@ -7,6 +7,9 @@ namespace SpriteTools;
 [GameResource("2D Sprite", "sprite", "A 2D sprite atlas", Icon = "emoji_emotions")]
 public class SpriteResource : GameResource
 {
+	/// <summary>
+	/// A list of animations that are available for this sprite.
+	/// </summary>
 	public List<SpriteAnimation> Animations { get; set; } = new()
 	{
 		new SpriteAnimation()
@@ -19,11 +22,37 @@ public class SpriteResource : GameResource
 
 public class SpriteAnimation
 {
-	[Property, Title("Animation Name")] private string _nameProp => Name;
-	public string Name { get; set; }
-	[Property, Range(0f, 999f, 0.01f, true, false), DefaultValue(15f)] public float FrameRate { get; set; } = 15.0f;
+	/// <summary>
+	/// The name of the animation. This is used as a key to reference the animation.
+	/// </summary>
+	[Property, Title("Animation Name")]
+	private string _nameProp => Name;
+
+	/// <summary>
+	/// The speed of the animation. This is the number of frames per second.
+	/// </summary>
+	[Property, Range(0f, 999f, 0.01f, true, false), DefaultValue(15f)]
+	public float FrameRate { get; set; } = 15.0f;
+
+	/// <summary>
+	/// The origin of the sprite. This is used to determine where the sprite is drawn relative to/scaled around.
+	/// </summary>
 	[Property, Range(0f, 1f, 0.01f, true, false), DefaultValue(0.5f)] public Vector2 Origin { get; set; } = new Vector2(0.5f, 0.5f);
-	[Property, DefaultValue(true)] public bool Looping { get; set; } = true;
+
+	/// <summary>
+	/// Whether or not the animation should loop.
+	/// </summary>
+	[Property, DefaultValue(true)]
+	public bool Looping { get; set; } = true;
+
+	/// <summary>
+	/// The name of the animation. This is used as a key to reference the animation.
+	/// </summary>
+	public string Name { get; set; }
+
+	/// <summary>
+	/// The list of frames that make up the animation. These are image paths.
+	/// </summary>
 	public List<string> Frames { get; set; }
 
 	public SpriteAnimation()
