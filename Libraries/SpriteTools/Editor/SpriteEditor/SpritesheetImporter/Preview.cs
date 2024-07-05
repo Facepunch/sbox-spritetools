53eb2ea620005a21d7b440f7435692e6ff392d44
Carson Kompon <carsokompo@gmail.com>
2024-07-05 09:58:06 -0400

    Remove Spritesheet Overlay

--- Libraries/SpriteTools/Editor/SpriteEditor/SpritesheetImporter/Preview.cs
+++ Libraries/SpriteTools/Editor/SpriteEditor/SpritesheetImporter/Preview.cs
@@ -21,44 +21,44 @@ public class Preview : Widget
         Rendering = new RenderingWidget(parent, this);
         Layout.Add(Rendering);
 
-        Overlay = new Widget(this)
-        {
-            Layout = Layout.Row(),
-            TranslucentBackground = true,
-            NoSystemBackground = true,
-            WindowFlags = WindowFlags.FramelessWindowHint | WindowFlags.Tool
-        };
-        overlayWindowZoom = new WidgetWindow(this);
-        overlayWindowZoom.Parent = Overlay;
-        overlayWindowZoom.Layout = Layout.Row();
-        overlayWindowZoom.Layout.Spacing = 4;
-        overlayWindowZoom.Layout.Margin = 4;
-        var btnZoomOut = overlayWindowZoom.Layout.Add(new IconButton("zoom_out"));
-        btnZoomOut.OnClick = () =>
-        {
-            Rendering.Zoom(-250);
-        };
-        btnZoomOut.ToolTip = "Zoom Out";
-        btnZoomOut.StatusTip = "Zoom Out View";
-        var btnZoomIn = overlayWindowZoom.Layout.Add(new IconButton("zoom_in"));
-        btnZoomIn.OnClick = () =>
-        {
-            Rendering.Zoom(250);
-        };
-        btnZoomIn.ToolTip = "Zoom In";
-        btnZoomIn.StatusTip = "Zoom In View";
-        overlayWindowZoom.Layout.AddSeparator();
-        var btnFit = overlayWindowZoom.Layout.Add(new IconButton("zoom_out_map"));
-        btnFit.OnClick = () =>
-        {
-            Rendering.Fit();
-        };
-        btnFit.ToolTip = "Fit to Screen";
-        btnFit.StatusTip = "Fit View to Screen";
-        overlayWindowZoom.WindowTitle = "Zoom Controls";
+        // Overlay = new Widget(this)
+        // {
+        //     Layout = Layout.Row(),
+        //     TranslucentBackground = true,
+        //     NoSystemBackground = true,
+        //     WindowFlags = WindowFlags.FramelessWindowHint | WindowFlags.Tool
+        // };
+        // overlayWindowZoom = new WidgetWindow(this);
+        // overlayWindowZoom.Parent = Overlay;
+        // overlayWindowZoom.Layout = Layout.Row();
+        // overlayWindowZoom.Layout.Spacing = 4;
+        // overlayWindowZoom.Layout.Margin = 4;
+        // var btnZoomOut = overlayWindowZoom.Layout.Add(new IconButton("zoom_out"));
+        // btnZoomOut.OnClick = () =>
+        // {
+        //     Rendering.Zoom(-250);
+        // };
+        // btnZoomOut.ToolTip = "Zoom Out";
+        // btnZoomOut.StatusTip = "Zoom Out View";
+        // var btnZoomIn = overlayWindowZoom.Layout.Add(new IconButton("zoom_in"));
+        // btnZoomIn.OnClick = () =>
+        // {
+        //     Rendering.Zoom(250);
+        // };
+        // btnZoomIn.ToolTip = "Zoom In";
+        // btnZoomIn.StatusTip = "Zoom In View";
+        // overlayWindowZoom.Layout.AddSeparator();
+        // var btnFit = overlayWindowZoom.Layout.Add(new IconButton("zoom_out_map"));
+        // btnFit.OnClick = () =>
+        // {
+        //     Rendering.Fit();
+        // };
+        // btnFit.ToolTip = "Fit to Screen";
+        // btnFit.StatusTip = "Fit View to Screen";
+        // overlayWindowZoom.WindowTitle = "Zoom Controls";
 
-        Overlay.Layout.Add(overlayWindowZoom);
-        Overlay.Show();
+        // Overlay.Layout.Add(overlayWindowZoom);
+        // Overlay.Show();
 
         var texture = Texture.Load(Sandbox.FileSystem.Mounted, parent.Path);
         if (texture is not null)
