56f83ccdba9aa5c9811588787f933478a7309f06
Carson Kompon <carsokompo@gmail.com>
2024-07-16 09:25:05 -0400

    Added Sprite Tools -> Flush Texture Cache to Menu

---
+++ Libraries/SpriteTools/Editor/ToolbarMenuOptions.cs
@@ -0,0 +1,16 @@
+using Sandbox;
+using Editor;
+using System.Threading.Tasks;
+
+namespace SpriteTools;
+
+public static class ToolbarMenuOptions
+{
+
+    [Menu("Editor", "Sprite Tools/Flush Texture Cache")]
+    public static void FlushTextureCache()
+    {
+        TextureAtlas.ClearCache();
+    }
+
+}
