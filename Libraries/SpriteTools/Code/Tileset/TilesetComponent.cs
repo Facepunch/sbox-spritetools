bbe2357183d4f1ccc4d8b92b259d5c03edd711ac
Carson Kompon <carsokompo@gmail.com>
2024-07-23 08:47:50 -0400

    Create TilesetSceneObject for drawing tiles in a single Graphics.Draw call.

--- Libraries/SpriteTools/Code/Tileset/TilesetComponent.cs
+++ Libraries/SpriteTools/Code/Tileset/TilesetComponent.cs
@@ -1,5 +1,7 @@
 using Sandbox;
+using System.Buffers;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SpriteTools;
 
@@ -12,8 +14,9 @@ public sealed class TilesetComponent : Component
 		public string Name { get; set; }
 		public bool IsVisible { get; set; }
 		public bool IsLocked { get; set; }
+		public TilesetResource TilesetResource { get; set; }
 
-		List<Tile> Tiles { get; set; } = new();
+		public List<Tile> Tiles { get; set; } = new();
 
 		public Layer(string name = "Untitled Layer")
 		{
@@ -37,27 +40,94 @@ public sealed class TilesetComponent : Component
 			}
 
 			return layer;
+		}
 
+		public void AddTile(Tile tile)
+		{
+			Tiles.Add(tile);
 		}
 	}
 
 	public class Tile
 	{
-		public TilesetResource TileResource { get; set; }
 		public string TileName { get; set; }
 		public Transform Transform { get; set; }
 
-		public Tile(TilesetResource tileResource, string tileName, Transform transform)
+		public Tile(string tileName, Transform transform)
 		{
-			TileResource = tileResource;
 			TileName = tileName;
 			Transform = transform;
 		}
 
 		public Tile Copy()
 		{
-			return new Tile(TileResource, TileName, Transform);
+			return new Tile(TileName, Transform);
 		}
 	}
 
+}
+
+internal sealed class TilesetSceneObject : SceneCustomObject
+{
+	TilesetComponent Component;
+	Material material;
+
+	public TilesetSceneObject(TilesetComponent component, SceneWorld world) : base(world)
+	{
+		Component = component;
+
+		material = Material.FromShader("materials/sprite_2d.vmat");
+	}
+
+	public override void RenderSceneObject()
+	{
+		if (Component?.Layers is null) return;
+
+		var layers = Component.Layers;
+		if (layers.Count == 0) return;
+
+		var viewerPosition = Graphics.CameraPosition;
+		var totalTiles = layers.Sum(x => x.Tiles.Count);
+		var vertex = ArrayPool<Vertex>.Shared.Rent(totalTiles * 4);
+
+		int i = 0;
+		foreach (var layer in layers)
+		{
+			if (!layer.IsVisible) continue;
+
+			var tileset = layer.TilesetResource;
+
+			foreach (var tile in layer.Tiles)
+			{
+				var tileName = tile.TileName;
+				var transform = tile.Transform;
+
+				var texture = Texture.Load(tileset.Atlas);
+				if (texture is null) continue;
+
+				var tiling = tileset.GetTiling();
+				var offset = tileset.GetOffset(4);
+
+				var position = transform.Position;
+				var size = transform.Scale;
+
+				var topLeft = new Vector3(position.x - size.x / 2, position.y - size.y / 2, position.z);
+				var topRight = new Vector3(position.x + size.x / 2, position.y - size.y / 2, position.z);
+				var bottomRight = new Vector3(position.x + size.x / 2, position.y + size.y / 2, position.z);
+				var bottomLeft = new Vector3(position.x - size.x / 2, position.y + size.y / 2, position.z);
+
+				var uvTopLeft = new Vector2(offset.x * tiling.x, offset.y * tiling.y);
+				var uvTopRight = new Vector2((offset.x + 1) * tiling.x, offset.y * tiling.y);
+				var uvBottomRight = new Vector2((offset.x + 1) * tiling.x, (offset.y + 1) * tiling.y);
+				var uvBottomLeft = new Vector2(offset.x * tiling.x, (offset.y + 1) * tiling.y);
+
+				vertex[i++] = new Vertex(topLeft);
+				vertex[i++] = new Vertex(topRight);
+				vertex[i++] = new Vertex(bottomRight);
+				vertex[i++] = new Vertex(bottomLeft);
+			}
+		}
+
+		Graphics.Draw(vertex, totalTiles * 4, material, Attributes);
+	}
 }
