d79bf0377771d4c793e35ddc628e5fb577876747
Carson Kompon <carsokompo@gmail.com>
2024-07-23 17:34:14 -0400

    Fixed TilesetComponent SceneObject vertex maths. Each layer now gets its own material, sharing materials between layers if same atlas is used.

--- Libraries/SpriteTools/Code/Tileset/TilesetComponent.cs
+++ Libraries/SpriteTools/Code/Tileset/TilesetComponent.cs
@@ -5,16 +5,52 @@ using System.Linq;
 
 namespace SpriteTools;
 
-public sealed class TilesetComponent : Component
+public sealed class TilesetComponent : Component, Component.ExecuteInEditor
 {
 	[Property, Group("Layers")] public List<Layer> Layers { get; set; }
+
+	TilesetSceneObject _so;
+
+	protected override void OnEnabled()
+	{
+		_so = new TilesetSceneObject(this, Scene.SceneWorld);
+		_so.Transform = Transform.World;
+		_so.Tags.SetFrom(Tags);
+	}
+
+	protected override void OnDisabled()
+	{
+		_so?.Delete();
+		_so = null;
+	}
+
+	protected override void OnTagsChanged()
+	{
+		_so?.Tags.SetFrom(Tags);
+	}
+
+	protected override void OnPreRender()
+	{
+		if (_so is null) return;
+		if (Layers.Count == 0)
+		{
+			_so.RenderingEnabled = false;
+			return;
+		}
+
+		_so.RenderingEnabled = true;
+		_so.Transform = Transform.World;
+		_so.Flags.CastShadows = false;
+		_so.Flags.IsOpaque = false;
+		_so.Flags.IsTranslucent = true;
+	}
 
 	public class Layer
 	{
 		public string Name { get; set; }
 		public bool IsVisible { get; set; }
 		public bool IsLocked { get; set; }
-		public TilesetResource TilesetResource { get; set; }
+		[Property, Group("Selected Layer")] public TilesetResource TilesetResource { get; set; }
 
 		public List<Tile> Tiles { get; set; } = new();
 
@@ -23,6 +59,11 @@ public sealed class TilesetComponent : Component
 			Name = name;
 			IsVisible = true;
 			IsLocked = false;
+
+			for (int i = 0; i < 10; i++)
+			{
+				Tiles.Add(new Tile("Tile " + i, new Transform((Vector3.Random.WithZ(0) * 64).SnapToGrid(1))));
+			}
 		}
 
 		public Layer Copy()
@@ -70,13 +111,11 @@ public sealed class TilesetComponent : Component
 internal sealed class TilesetSceneObject : SceneCustomObject
 {
 	TilesetComponent Component;
-	Material material;
+	Dictionary<string, Material> Materials = new();
 
 	public TilesetSceneObject(TilesetComponent component, SceneWorld world) : base(world)
 	{
 		Component = component;
-
-		material = Material.FromShader("materials/sprite_2d.vmat");
 	}
 
 	public override void RenderSceneObject()
@@ -87,47 +126,95 @@ internal sealed class TilesetSceneObject : SceneCustomObject
 		if (layers.Count == 0) return;
 
 		var viewerPosition = Graphics.CameraPosition;
-		var totalTiles = layers.Sum(x => x.Tiles.Count);
-		var vertex = ArrayPool<Vertex>.Shared.Rent(totalTiles * 4);
 
-		int i = 0;
 		foreach (var layer in layers)
 		{
 			if (!layer.IsVisible) continue;
 
+			int i = 0;
+			var totalTiles = layer.Tiles.Count;
+			var vertex = ArrayPool<Vertex>.Shared.Rent(totalTiles * 6);
+
 			var tileset = layer.TilesetResource;
+			var material = GetMaterial(tileset.Atlas);
 
 			foreach (var tile in layer.Tiles)
 			{
 				var tileName = tile.TileName;
 				var transform = tile.Transform;
 
-				var texture = Texture.Load(tileset.Atlas);
-				if (texture is null) continue;
-
 				var tiling = tileset.GetTiling();
 				var offset = tileset.GetOffset(4);
 
-				var position = transform.Position;
-				var size = transform.Scale;
+				var position = transform.Position * tileset.TileSize;
+				var size = transform.Scale * tileset.TileSize;
 
-				var topLeft = new Vector3(position.x - size.x / 2, position.y - size.y / 2, position.z);
-				var topRight = new Vector3(position.x + size.x / 2, position.y - size.y / 2, position.z);
-				var bottomRight = new Vector3(position.x + size.x / 2, position.y + size.y / 2, position.z);
-				var bottomLeft = new Vector3(position.x - size.x / 2, position.y + size.y / 2, position.z);
+				var topLeft = new Vector3(position.x, position.y, position.z);
+				var topRight = new Vector3(position.x + size.x, position.y, position.z);
+				var bottomRight = new Vector3(position.x + size.x, position.y + size.y, position.z);
+				var bottomLeft = new Vector3(position.x, position.y + size.y, position.z);
 
 				var uvTopLeft = new Vector2(offset.x * tiling.x, offset.y * tiling.y);
 				var uvTopRight = new Vector2((offset.x + 1) * tiling.x, offset.y * tiling.y);
 				var uvBottomRight = new Vector2((offset.x + 1) * tiling.x, (offset.y + 1) * tiling.y);
 				var uvBottomLeft = new Vector2(offset.x * tiling.x, (offset.y + 1) * tiling.y);
 
-				vertex[i++] = new Vertex(topLeft);
-				vertex[i++] = new Vertex(topRight);
-				vertex[i++] = new Vertex(bottomRight);
-				vertex[i++] = new Vertex(bottomLeft);
+				vertex[i] = new Vertex(topLeft);
+				vertex[i].TexCoord0 = uvTopLeft;
+				vertex[i].TexCoord1 = new Vector4(0, 0, 0, 0);
+				vertex[i].Color = Color.White;
+				vertex[i].Normal = Vector3.Up;
+				i++;
+
+				vertex[i] = new Vertex(topRight);
+				vertex[i].TexCoord0 = uvTopRight;
+				vertex[i].TexCoord1 = new Vector4(0, 0, 0, 0);
+				vertex[i].Color = Color.White;
+				vertex[i].Normal = Vector3.Up;
+				i++;
+
+				vertex[i] = new Vertex(bottomRight);
+				vertex[i].TexCoord0 = uvBottomRight;
+				vertex[i].TexCoord1 = new Vector4(0, 0, 0, 0);
+				vertex[i].Color = Color.White;
+				vertex[i].Normal = Vector3.Up;
+				i++;
+
+				vertex[i] = new Vertex(topLeft);
+				vertex[i].TexCoord0 = uvTopLeft;
+				vertex[i].TexCoord1 = new Vector4(0, 0, 0, 0);
+				vertex[i].Color = Color.White;
+				vertex[i].Normal = Vector3.Up;
+				i++;
+
+				vertex[i] = new Vertex(bottomRight);
+				vertex[i].TexCoord0 = uvBottomRight;
+				vertex[i].TexCoord1 = new Vector4(0, 0, 0, 0);
+				vertex[i].Color = Color.White;
+				vertex[i].Normal = Vector3.Up;
+				i++;
+
+				vertex[i] = new Vertex(bottomLeft);
+				vertex[i].TexCoord0 = uvBottomLeft;
+				vertex[i].TexCoord1 = new Vector4(0, 0, 0, 0);
+				vertex[i].Color = Color.White;
+				vertex[i].Normal = Vector3.Up;
+				i++;
 			}
+
+			Graphics.Draw(vertex, totalTiles * 6, material, Attributes);
+		}
+	}
+
+	Material GetMaterial(string texturePath)
+	{
+		if (!Materials.TryGetValue(texturePath, out var material))
+		{
+			material = Material.Load("materials/sprite_2d.vmat");
+			material.Set("Texture", Texture.Load(FileSystem.Mounted, texturePath));
+			Materials.Add(texturePath, material);
 		}
 
-		Graphics.Draw(vertex, totalTiles * 4, material, Attributes);
+		return material;
 	}
 }
