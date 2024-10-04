f2978545d82682c1bd4de20b00bb470c0b2af233
Carson Kompon <carsokompo@gmail.com>
2024-10-04 14:05:07 -0400

    Added SpriteResource.CopyFrom

--- Libraries/SpriteTools/Code/Sprite/SpriteResource.cs
+++ Libraries/SpriteTools/Code/Sprite/SpriteResource.cs
@@ -76,6 +76,33 @@ public class SpriteResource : GameResource
 		return paths;
 	}
 
+	/// <summary>
+	/// Copies all the data from another sprite resource into this one.
+	/// </summary>
+	/// <param name="otherSprite"></param>
+	public void CopyFrom(SpriteResource otherSprite)
+	{
+		Animations = new List<SpriteAnimation>();
+		foreach (var otherAnimation in otherSprite.Animations)
+		{
+			var newAnimation = new SpriteAnimation(otherAnimation.Name)
+			{
+				FrameRate = otherAnimation.FrameRate,
+				Looping = otherAnimation.Looping,
+				Origin = otherAnimation.Origin
+			};
+			foreach (var otherFrame in otherAnimation.Frames)
+			{
+				newAnimation.Frames.Add(otherFrame.Copy());
+			}
+			foreach (var otherAttachment in otherAnimation.Attachments)
+			{
+				newAnimation.Attachments.Add(otherAttachment.Copy());
+			}
+			Animations.Add(newAnimation);
+		}
+	}
+
 }
 
 public class SpriteAnimation
