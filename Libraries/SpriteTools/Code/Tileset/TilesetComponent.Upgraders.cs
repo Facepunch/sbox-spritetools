0df2870d3add777d5829a6a685630440afcb040f
Carson Kompon <carsokompo@gmail.com>
2024-11-19 09:00:03 -0500

    Remove logs

--- Libraries/SpriteTools/Code/Tileset/TilesetComponent.Upgraders.cs
+++ Libraries/SpriteTools/Code/Tileset/TilesetComponent.Upgraders.cs
@@ -14,27 +14,21 @@ public partial class TilesetComponent
     [JsonUpgrader(typeof(TilesetComponent), 1)]
     static void Upgrader_v1(JsonObject json)
     {
-        Log.Info("we upgrading...");
-
         if (json.ContainsKey("LayerDistance"))
         {
             var distance = json["LayerDistance"].GetValue<float>();
-            Log.Info("Distance found!");
 
             if (json.ContainsKey("Layers"))
             {
-                Log.Info("we have Layers...");
                 var layerList = json["Layers"].AsArray();
                 for (int i = 0; i < layerList.Count; i++)
                 {
-                    Log.Info("Checking a layer..");
                     var layer = layerList[i];
                     if (layer is null) continue;
                     var layerObj = layer.AsObject();
                     layerObj["Height"] = distance * i;
                 }
             }
-            Log.Info("Jobs done!");
         }
     }
 }
