1afe3f7811cd6efc26613919851f9e0555fe8691
Carson Kompon <carsokompo@gmail.com>
2025-05-14 11:23:42 -0400

    Fix LoopMode not serializing correctly on SpriteResource, causing errors when saying "No" to saving changes.

--- Libraries/SpriteTools/Code/Sprite/SpriteResource.cs
+++ Libraries/SpriteTools/Code/Sprite/SpriteResource.cs
@@ -1,10 +1,11 @@
 ﻿using Sandbox;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 
 namespace SpriteTools;
 
-[GameResource("2D Sprite", "sprite", "A 2D sprite atlas", Icon = "emoji_emotions")]
+[GameResource( "2D Sprite", "sprite", "A 2D sprite atlas", Icon = "emoji_emotions" )]
 public partial class SpriteResource : GameResource
 {
 	/// <summary>
@@ -23,23 +24,23 @@ public partial class SpriteResource : GameResource
 	/// </summary>
 	/// <param name="name"></param>
 	/// <returns></returns>
-	public SpriteAnimation GetAnimation(string name)
+	public SpriteAnimation GetAnimation ( string name )
 	{
-		return Animations.FirstOrDefault(x => x.Name == name);
+		return Animations.FirstOrDefault( x => x.Name == name );
 	}
 
 	/// <summary>
 	/// Returns a list of names for every attachment this Sprite has.
 	/// </summary>
-	public List<string> GetAttachmentNames()
+	public List<string> GetAttachmentNames ()
 	{
 		var attachmentNames = new List<string>();
-		foreach (var animation in Animations)
+		foreach ( var animation in Animations )
 		{
-			foreach (var attachment in animation.Attachments)
+			foreach ( var attachment in animation.Attachments )
 			{
-				if (!attachmentNames.Contains(attachment.Name))
-					attachmentNames.Add(attachment.Name);
+				if ( !attachmentNames.Contains( attachment.Name ) )
+					attachmentNames.Add( attachment.Name );
 			}
 		}
 		return attachmentNames;
@@ -49,37 +50,37 @@ public partial class SpriteResource : GameResource
 	/// Try to load a sprite from a file path.
 	/// </summary>
 	/// <param name="path">The path to the sprite resource</param>
-	public static SpriteResource Load(string path)
+	public static SpriteResource Load ( string path )
 	{
-		return ResourceLibrary.Get<SpriteResource>(path);
+		return ResourceLibrary.Get<SpriteResource>( path );
 	}
 
 	/// <summary>
 	/// Returns the first frame of a sprite resource as a texture.
 	/// </summary>
 	/// <returns></returns>
-	public Texture GetPreviewTexture()
+	public Texture GetPreviewTexture ()
 	{
 		var anim = Animations.FirstOrDefault();
-		if (anim is null || anim.Frames.Count == 0) return Texture.Transparent;
-		if (anim.Frames.Count == 1) return Texture.Load(FileSystem.Mounted, anim.Frames[0].FilePath);
-		var atlas = TextureAtlas.FromAnimation(anim);
-		return atlas.GetTextureFromFrame(0);
+		if ( anim is null || anim.Frames.Count == 0 ) return Texture.Transparent;
+		if ( anim.Frames.Count == 1 ) return Texture.Load( FileSystem.Mounted, anim.Frames[0].FilePath );
+		var atlas = TextureAtlas.FromAnimation( anim );
+		return atlas.GetTextureFromFrame( 0 );
 	}
 
 	/// <summary>
 	/// Returns a list of all the texture paths used by this sprite.
 	/// </summary>
 	/// <returns></returns>
-	public List<string> GetAllTexturePaths()
+	public List<string> GetAllTexturePaths ()
 	{
 		var paths = new List<string>();
-		foreach (var animation in Animations)
+		foreach ( var animation in Animations )
 		{
-			foreach (var frame in animation.Frames)
+			foreach ( var frame in animation.Frames )
 			{
-				if (paths.Contains(frame.FilePath)) continue;
-				paths.Add(frame.FilePath);
+				if ( paths.Contains( frame.FilePath ) ) continue;
+				paths.Add( frame.FilePath );
 			}
 		}
 		return paths;
@@ -93,19 +94,19 @@ public partial class SpriteResource : GameResource
 		/// <summary>
 		/// The animation will play from start to finish and then stop.
 		/// </summary>
-		[Icon("not_interested")]
+		[Icon( "not_interested" )]
 		None,
 
 		/// <summary>
 		/// The animation will play from start to finish and then loop back to the start.
 		/// </summary>
-		[Icon("loop"), Title("Loop")]
+		[Icon( "loop" ), Title( "Loop" )]
 		Forward,
 
 		/// <summary>
 		/// The animation will play from start to finish and then backwards from finish to start before looping.
 		/// </summary>
-		[Icon("sync_alt")]
+		[Icon( "sync_alt" )]
 		PingPong
 	}
 
@@ -116,31 +117,38 @@ public class SpriteAnimation
 	/// <summary>
 	/// The name of the animation. This is used as a key to reference the animation.
 	/// </summary>
-	[Property, Title("Animation Name")]
+	[Property, Title( "Animation Name" )]
 	private string _nameProp => Name;
 
 	/// <summary>
 	/// The speed of the animation. This is the number of frames per second.
 	/// </summary>
-	[Property, Range(0f, 999f, 0.01f, true, false)]
+	[Property, Range( 0f, 999f, 0.01f, true, false )]
 	public float FrameRate { get; set; } = 15.0f;
 
 	/// <summary>
 	/// The origin of the sprite. This is used to determine where the sprite is drawn relative to/scaled around.
 	/// </summary>
-	[Property, Range(0f, 1f, 0.01f, true, false)] public Vector2 Origin { get; set; } = new Vector2(0.5f, 0.5f);
+	[Property, Range( 0f, 1f, 0.01f, true, false )] public Vector2 Origin { get; set; } = new Vector2( 0.5f, 0.5f );
 
 	/// <summary>
 	/// Whether or not the animation should loop. Replaced with LoopMode.
 	/// </summary>
-	[System.Obsolete("Use LoopMode instead.")]
+	[System.Obsolete( "Use LoopMode instead." )]
 	public bool Looping { get; set; } = false;
 
 	/// <summary>
 	/// Whether or not the animation should loop and how.
 	/// </summary>
-	[Property, Title("Looping")]
+	[Property, Title( "Looping" ), JsonIgnore]
 	public SpriteResource.LoopMode LoopMode { get; set; } = SpriteResource.LoopMode.Forward;
+
+	[Hide, JsonInclude]
+	private int LoopModeVal
+	{
+		get => (int)LoopMode;
+		set => LoopMode = (SpriteResource.LoopMode)value;
+	}
 
 	/// <summary>
 	/// The name of the animation. This is used as a key to reference the animation.
@@ -160,37 +168,37 @@ public class SpriteAnimation
 	public int? LoopStart { get; set; } = null;
 	public int? LoopEnd { get; set; } = null;
 
-	public SpriteAnimation()
+	public SpriteAnimation ()
 	{
 		Frames = new List<SpriteAnimationFrame>();
 		Attachments = new List<SpriteAttachment>();
 	}
 
-	public SpriteAnimation(string name)
+	public SpriteAnimation ( string name )
 	{
 		Name = name;
 		Frames = new List<SpriteAnimationFrame>();
 		Attachments = new List<SpriteAttachment>();
 	}
 
-	public int GetLoopStart()
+	public int GetLoopStart ()
 	{
-		if (LoopStart is null) return 0;
+		if ( LoopStart is null ) return 0;
 		return LoopStart.Value;
 	}
 
-	public int GetLoopEnd()
+	public int GetLoopEnd ()
 	{
-		if (LoopEnd is null) return Frames.Count - 1;
+		if ( LoopEnd is null ) return Frames.Count - 1;
 		return LoopEnd.Value;
 	}
 
-	public Vector2 GetAttachmentPosition(string attachment, int index)
+	public Vector2 GetAttachmentPosition ( string attachment, int index )
 	{
-		var attach = Attachments?.FirstOrDefault(x => x.Name == attachment);
-		if (attach is null) return Vector2.Zero;
-		if (index < 0) return attach.Points.FirstOrDefault();
-		if (index >= attach.Points.Count) return attach.Points.LastOrDefault();
+		var attach = Attachments?.FirstOrDefault( x => x.Name == attachment );
+		if ( attach is null ) return Vector2.Zero;
+		if ( index < 0 ) return attach.Points.FirstOrDefault();
+		if ( index >= attach.Points.Count ) return attach.Points.LastOrDefault();
 		return attach.Points[index];
 	}
 }
@@ -201,17 +209,17 @@ public class SpriteAnimationFrame
 	public List<string> Events { get; set; }
 	public Rect SpriteSheetRect { get; set; }
 
-	public SpriteAnimationFrame(string filePath)
+	public SpriteAnimationFrame ( string filePath )
 	{
 		FilePath = filePath;
 		Events = new List<string>();
-		SpriteSheetRect = new Rect(0, 0, 0, 0);
+		SpriteSheetRect = new Rect( 0, 0, 0, 0 );
 	}
 
-	public SpriteAnimationFrame Copy()
+	public SpriteAnimationFrame Copy ()
 	{
-		var copy = new SpriteAnimationFrame(FilePath);
-		copy.Events = new List<string>(Events);
+		var copy = new SpriteAnimationFrame( FilePath );
+		copy.Events = new List<string>( Events );
 		copy.SpriteSheetRect = SpriteSheetRect;
 		return copy;
 	}
@@ -239,7 +247,7 @@ public class SpriteAttachment
 	/// </summary>
 	public bool Visible { get; set; }
 
-	public SpriteAttachment()
+	public SpriteAttachment ()
 	{
 		Name = "new attachment";
 		Color = Color.Red;
@@ -247,7 +255,7 @@ public class SpriteAttachment
 		Visible = true;
 	}
 
-	public SpriteAttachment(string name)
+	public SpriteAttachment ( string name )
 	{
 		Name = name;
 		Color = Color.Red;
@@ -255,11 +263,11 @@ public class SpriteAttachment
 		Visible = true;
 	}
 
-	public SpriteAttachment Copy()
+	public SpriteAttachment Copy ()
 	{
-		var copy = new SpriteAttachment(Name);
+		var copy = new SpriteAttachment( Name );
 		copy.Color = Color;
-		copy.Points = new List<Vector2>(Points);
+		copy.Points = new List<Vector2>( Points );
 		copy.Visible = Visible;
 		return copy;
 	}
